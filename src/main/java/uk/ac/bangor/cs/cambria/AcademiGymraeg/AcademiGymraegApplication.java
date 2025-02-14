b8c17138efb036803223b10fc30ae6d877b70df8
jonesjacklewis <jones.jack.lewis@gmail.com>
2025-02-14 14:54:22 +0000

    first commit

---
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/AcademiGymraegApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cs.cambria.AcademiGymraeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiGymraegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiGymraegApplication.class, args);
+	}
+
+}
