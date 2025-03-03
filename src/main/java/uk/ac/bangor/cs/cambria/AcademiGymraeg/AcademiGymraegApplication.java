e6f8291cefc23b75d302d29b3da874e7d15d87fd
peterkeithgriffiths@gmail.com <98642969+PeteTech1990@users.noreply.github.com>
2025-03-03 14:06:12 +0000

    Gitignore Peter application properies

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/AcademiGymraegApplication.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/AcademiGymraegApplication.java
@@ -2,6 +2,7 @@ package uk.ac.bangor.cs.cambria.AcademiGymraeg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class AcademiGymraegApplication {
