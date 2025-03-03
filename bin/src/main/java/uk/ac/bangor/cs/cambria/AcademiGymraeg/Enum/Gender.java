865786811dc153c9bc5f9e18c242036afa9fabb4
peterkeithgriffiths@gmail.com <98642969+PeteTech1990@users.noreply.github.com>
2025-03-03 15:34:37 +0000

    Added JavaDoc comments

--- bin/src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/Enum/Gender.java
+++ bin/src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/Enum/Gender.java
@@ -4,7 +4,8 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg.Enum;
 
 /**
- * 
+ * This enum is used to classify the gender of a welsh noun, either masculine, feminine or plural. 
+ * In the database, this classification will be represented by a tinyint column, in the Noun table.
  */
 public enum Gender {
 		FEMININE,
