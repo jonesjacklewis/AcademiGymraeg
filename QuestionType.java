89c7ccf3ca2ee22d1224b94f3499adf35ca7f9d5
dwp22pzv <dwp22pzv@OADS20630.ad.bangor.ac.uk>
2025-03-10 16:55:33 +0000

    Further work on Question class. Created QuestionType enum.

---
+++ QuestionType.java
@@ -0,0 +1,13 @@
+
+package uk.ac.bangor.cs.cambria.AcademiGymraeg.Enum;
+
+/**
+ * This enum is used to classify the type of a question. 
+ * In the database, this classification will be represented by a tinyint column, in the Question table.
+ */
+public enum QuestionType {
+		ENGLISH_TO_WELSH,
+		WELSH_TO_ENGLISH,
+		GENDER
+	
+}
