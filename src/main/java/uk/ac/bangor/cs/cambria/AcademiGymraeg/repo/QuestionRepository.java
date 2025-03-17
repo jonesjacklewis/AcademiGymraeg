28ff72c0e505d9e853812afb00140d4feba0ac13
dwp22pzv <dwp22pzv@OADS20630.ad.bangor.ac.uk>
2025-03-17 16:41:05 +0000

    Added auth thingy

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/repo/QuestionRepository.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/repo/QuestionRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import uk.ac.bangor.cs.cambria.AcademiGymraeg.Model.Question;
 
+/**
+ * @author dwp22pzv
+ */
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 }
