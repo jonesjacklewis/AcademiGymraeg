96bd798e0c336abcceff83edaa53bbeed42ec74f
3-TOM <22042828@cambria.ac.uk>
2025-03-17 16:57:56 +0000

    Add files via upload

---
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/repo/TestRepository.java
@@ -0,0 +1,13 @@
+package uk.ac.bangor.cs.cambria.AcademiGymraeg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.Model.Test;
+
+/**
+ * @author thh21bgf
+ */
+
+public interface TestRepository extends JpaRepository<Test, Long>{
+
+}
