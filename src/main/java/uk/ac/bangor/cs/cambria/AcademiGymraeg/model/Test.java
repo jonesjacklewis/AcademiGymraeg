af8f68da25dbe084d77a7e01bf84e052c8770c09
3-TOM <22042828@cambria.ac.uk>
2025-03-24 16:31:07 +0000

    Added User to Test Class

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/model/Test.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/model/Test.java
@@ -3,7 +3,6 @@ package uk.ac.bangor.cs.cambria.AcademiGymraeg.model;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,15 +20,7 @@ public class Test {
 
 	@Transient
 	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long testId;
-	
-	@NotNull
-	@Column(nullable = false, updatable = false)
-	private ZonedDateTime startDateTime;
-	
+
 	@NotNull
 	@Column(nullable = false, updatable = false)
 	private ZonedDateTime endDateTime;
@@ -37,57 +28,77 @@ public class Test {
 	@NotNull
 	@Column(nullable = false, updatable = false)
 	private int numberCorrect;
-	
+
 	@NotNull
 	@Column(nullable = false, updatable = false)
 	private int numberOfQuestions;
-	
-	public ZonedDateTime getStartDateTime() {
-		return startDateTime;
-	}
-	
-	public void setStartDateTime(ZonedDateTime startDateTime) {
-		this.startDateTime = startDateTime;
-	}
-	
+
+	@NotNull
+	@Column(nullable = false, updatable = false)
+	private ZonedDateTime startDateTime;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long testId;
+
+	@NotNull
+	@Column(nullable = false, updatable = false)
+	private User user;
+
 	public ZonedDateTime getEndDateTime() {
 		return endDateTime;
 	}
-	
-	public void setEndDateTime(ZonedDateTime endDateTime) {
-		this.endDateTime = endDateTime;
-	}
-	
-	public String getFormattedStartTime() {
-		return formatter.format(startDateTime);
-	}
-	
+
 	public String getFormattedEndTime() {
 		return formatter.format(endDateTime);
 	}
-	
-	public Long getTestId() {
-		return testId;
+
+	public String getFormattedStartTime() {
+		return formatter.format(startDateTime);
 	}
-	
-	public void setTestId(Long testId) {
-		this.testId = testId;
-	}
-	
+
 	public int getNumberCorrect() {
 		return numberCorrect;
 	}
-	
-	public void setNumberCorrect(int numberCorrect) {
-		this.numberCorrect = numberCorrect;
-	}
-	
+
 	public int getNumberOfQuestions() {
 		return numberOfQuestions;
 	}
-	
+
+	public ZonedDateTime getStartDateTime() {
+		return startDateTime;
+	}
+
+	public Long getTestId() {
+		return testId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setEndDateTime(ZonedDateTime endDateTime) {
+		this.endDateTime = endDateTime;
+	}
+
+	public void setNumberCorrect(int numberCorrect) {
+		this.numberCorrect = numberCorrect;
+	}
+
 	public void setNumberOfQuestions(int numberOfQuestions) {
 		this.numberOfQuestions = numberOfQuestions;
 	}
-	
+
+	public void setStartDateTime(ZonedDateTime startDateTime) {
+		this.startDateTime = startDateTime;
+	}
+
+	public void setTestId(Long testId) {
+		this.testId = testId;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 }
