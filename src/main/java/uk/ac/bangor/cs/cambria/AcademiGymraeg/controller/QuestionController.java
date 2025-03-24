e44ab6df973a022637cb79631fc36f0592c2b95a
dwp22pzv <dwp22pzv@OADS20630.ad.bangor.ac.uk>
2025-03-24 16:37:24 +0000

    Created questionController stub

---
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/QuestionController.java
@@ -0,0 +1,12 @@
+package uk.ac.bangor.cs.cambria.AcademiGymraeg.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author dwp22pzv
+ */
+
+@Controller
+public class QuestionController {
+
+}
