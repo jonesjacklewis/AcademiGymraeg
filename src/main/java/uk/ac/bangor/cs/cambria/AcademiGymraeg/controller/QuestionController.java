1643c7381f7d1752d65be3f274a3239f5db47106
dwp22pzv <dwp22pzv@OADS20630.ad.bangor.ac.uk>
2025-03-24 16:56:56 +0000

    Trying to get random questions from repo

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/QuestionController.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/QuestionController.java
@@ -1,6 +1,9 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.repo.QuestionRepository;
 
 /**
  * @author dwp22pzv
@@ -8,5 +11,12 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class QuestionController {
+
+	@Autowired
+	private QuestionRepository repo;
+	
+	//https://stackoverflow.com/questions/24279186/fetch-random-records-using-spring-data-jpa
+	
+	//private List<Noun> = repo.
 
 }
