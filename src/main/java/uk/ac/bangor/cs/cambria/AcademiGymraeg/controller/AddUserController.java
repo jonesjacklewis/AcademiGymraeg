71aa51da804e92cfb355d46d6e46234a6d4cc1ef
garethsalomon <162130219+garethsalomon@users.noreply.github.com>
2025-03-25 20:58:22 +0000

    Add new user registration form

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/AddUserController.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/AddUserController.java
@@ -1,16 +1,36 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * @author grs22lkc
- */
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.model.User;
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.repo.UserRepository;
 
 @Controller
 public class AddUserController {
-	@GetMapping("/addUser")
-	public String addUserPage() {
-		return "useradmin";
-	}
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/useradmin")
+    public String showUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "useradmin";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user, 
+                          @RequestParam(value = "admin", required = false, defaultValue = "false") boolean admin,
+                          @RequestParam(value = "instructor", required = false, defaultValue = "false") boolean instructor,
+                          Model model) {
+        user.setAdmin(admin);
+        user.setInstructor(instructor);
+        userRepository.save(user);
+        model.addAttribute("message", "User added successfully!");
+        return "redirect:/useradmin";
+    }
 }
