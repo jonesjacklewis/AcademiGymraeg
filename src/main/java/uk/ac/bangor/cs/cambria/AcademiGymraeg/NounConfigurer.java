de577e0957f2574e36c4c12bc23793cbbfa964cb
3-TOM <22042828@cambria.ac.uk>
2025-03-31 16:55:13 +0100

    Load nouns from csv file

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/NounConfigurer.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/NounConfigurer.java
@@ -1,6 +1,13 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -8,41 +15,62 @@ import uk.ac.bangor.cs.cambria.AcademiGymraeg.enums.Gender;
 import uk.ac.bangor.cs.cambria.AcademiGymraeg.model.Noun;
 import uk.ac.bangor.cs.cambria.AcademiGymraeg.repo.NounRepository;
 
+/**
+ * @author jcj23xfb, thh21bgf
+ */
+
 @Component
 public class NounConfigurer {
 
 	@Autowired
 	private NounRepository repo;
-	
+
+	@Value("classpath:/nouns.csv")
+	Resource csvFile;
+
 	@PostConstruct
 	void setupSomeNouns() {
-		
-		Noun n = new Noun();
-		n.setEnglishNoun("Aeroplane");
-		n.setWelshNoun("Awyren");
-		n.setGender(Gender.FEMININE);
-		repo.save(n);
-		
-		n = new Noun();
-		n.setEnglishNoun("Apple");
-		n.setWelshNoun("Afal");
-		n.setGender(Gender.MASCULINE);
-		repo.save(n);
-	
-		
-		n = new Noun();
-		n.setEnglishNoun("Book");
-		n.setWelshNoun("Llyfr");
-		n.setGender(Gender.MASCULINE);
-		repo.save(n);
-	
-		
-		n = new Noun();
-		n.setEnglishNoun("Business");
-		n.setWelshNoun("Busnes");
-		n.setGender(Gender.MASCULINE);
-		repo.save(n);
-	
-		
+		if (repo.count() > 0)
+			return;
+
+		if (csvFile == null)
+			return;
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(csvFile.getInputStream()))) {
+			String line;
+
+			br.readLine();
+
+			while ((line = br.readLine()) != null) {
+
+				if (!"English,Welsh,Gender".equals(line.trim())) {
+
+					String[] lineParts = line.split(",");
+
+					Gender gender = null;
+
+					switch (lineParts[2].trim()) {
+					case "Masculine":
+						gender = Gender.MASCULINE;
+						break;
+					case "Feminine":
+						gender = Gender.FEMININE;
+						break;
+					}
+
+					if (gender != null)
+						repo.save(new Noun(lineParts[0], lineParts[1], gender));
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
 }
--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/repo/TestRepository.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/repo/TestRepository.java
@@ -1,5 +1,8 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg.repo;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import uk.ac.bangor.cs.cambria.AcademiGymraeg.model.Test;
@@ -9,5 +12,9 @@ import uk.ac.bangor.cs.cambria.AcademiGymraeg.model.Test;
  */
 
 public interface TestRepository extends JpaRepository<Test, Long>{
-
+	
+	Optional <Test> findByTestId(Long testId);
+	Optional <Test> findTestEndDateTimeBetween(ZonedDateTime endDateTime);
+	Optional <Test> findByNumberCorrect(int numberCorrect);
+	
 }
