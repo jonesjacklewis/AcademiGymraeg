1ff938a5b578be466c34f31bb2fd3b0b26a45dc4
Pete Griffiths <98642969+Pete1990Griff@users.noreply.github.com>
2025-03-31 09:22:02 +0100

    Add files via upload
    Added ability to create a noun, and list all Nouns. Still to do: Delete a Noun

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/NounController.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/NounController.java
@@ -1,32 +1,61 @@
 package uk.ac.bangor.cs.cambria.AcademiGymraeg.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.enums.Gender;
 import uk.ac.bangor.cs.cambria.AcademiGymraeg.model.Noun;
+import uk.ac.bangor.cs.cambria.AcademiGymraeg.repo.NounRepository;
 
 /**
  * @author ptg22svs
  */
 
 @Controller
+@RequestMapping("/noun")
 public class NounController {
+			
+	@Autowired
+	private NounRepository repo;
 	
-	@GetMapping("/noun")	
+	private List<Gender> genders = Arrays.asList(Gender.values());
+	
+	@GetMapping
 	public String nounAdminPage(Model m) {
 		m.addAttribute("noun", new Noun());
 		
+		m.addAttribute("genders", genders);
+		
+		m.addAttribute("allnouns", repo.findAll());
+
 		return "nounadmin";
 	}
-	
-	
-	@PostMapping
-	public String newNoun(BindingResult result, Model m) {
 
-		
-	}
+	
+	  @PostMapping 
+	  public String newnoun(Noun n, BindingResult result, Model m) {
+		  repo.save(n);
+		  
+		  return nounAdminPage(m);
+	  
+	  }
+	  
+	  @GetMapping("/deletenoun/{id}")
+	  @PreAuthorize("hasRole('ADMIN')")	  
+	  public String deletenoun(@PathVariable("id") Long id)
+	  {
+		  repo.deleteById(id);
+		  return "redirect:/noun";
+	  }
+	 
 }
-
