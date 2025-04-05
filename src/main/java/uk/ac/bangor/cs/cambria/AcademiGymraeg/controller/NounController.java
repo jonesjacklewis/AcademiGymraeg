71870569ce5812da0bc8fdd191535100b5d96f8c
peterkeithgriffiths@gmail.com <98642969+PeteTech1990@users.noreply.github.com>
2025-04-05 10:40:35 +0100

    Added ability to edit nouns

--- src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/NounController.java
+++ src/main/java/uk/ac/bangor/cs/cambria/AcademiGymraeg/controller/NounController.java
@@ -21,15 +21,28 @@ import uk.ac.bangor.cs.cambria.AcademiGymraeg.repo.NounRepository;
  * @author ptg22svs
  */
 
+
+/**
+ * All requests to "/noun" (and subdocs) are handled by this controller
+ */
+
 @Controller
 @RequestMapping("/noun")
 public class NounController {
-			
+	
+	//TODO - Add error handling
+	//TODO - Add confirmation messages for additions, changes or deletions
+	
 	@Autowired
 	private NounRepository repo;
 	
 	private List<Gender> genders = Arrays.asList(Gender.values());
 	
+	
+	//GET requests to "/noun" (Initial request) - new Noun object is passed in, 
+	//in preparation to adding a new noun to the data store.
+	//All GENDER enum values are also passed in, to display as radio button options on the New Noun form.
+	//All existing noun objects are also passed in as a List, to display in the All Nouns table.
 	@GetMapping
 	public String nounAdminPage(Model m) {
 		m.addAttribute("noun", new Noun());
@@ -42,21 +55,27 @@ public class NounController {
 	}
 
 	
+	//POST requests to "/noun". Handles validating entry into the New Noun form, before creating a 
+	//new Noun entry in the data store
 	  @PostMapping 
 	  public String newNoun(Noun n, BindingResult result, Model m) throws Exception {
 		  
-		  //Validate what has been entered - alphabet only, lowercase, no spaces
-		  
-		  String regex = "([a-z])\\w*([^A-Z])*";
+		//Validate what has been entered - alphabet only, no spaces
+		  	  
+		  String regex = "([a-zA-Z])\\w*";
 		  
 		  if(!n.getEnglishNoun().matches(regex))
 		  {
-			  throw new Exception("Invalid input for English noun. Only lowercase letters allowed");
+			  throw new Exception("Invalid input for English noun. Only letters allowed, no spaces");
 		  }
 		  else if(!n.getWelshNoun().matches(regex))
 		  {
-			  throw new Exception("Invalid input for Welsh noun. Only lowercase letters allowed");
+			  throw new Exception("Invalid input for Welsh noun. Only letters allowed, no spaces");
 		  }
+		  
+		  //Convert to lowercase
+		  n.setEnglishNoun(n.getEnglishNoun().toLowerCase());
+		  n.setWelshNoun(n.getWelshNoun().toLowerCase());
 		  
 		  repo.save(n);
 		  
@@ -64,6 +83,7 @@ public class NounController {
 	  
 	  }
 	  
+	  //GET requests to "/noun/deletenoun/{nounID}. Handles deleting a requested Noun from the data store.
 	  @GetMapping("/deletenoun/{id}")
 	  public String deletenoun(@PathVariable("id") Long id)
 	  {
@@ -71,30 +91,38 @@ public class NounController {
 		  return "redirect:/noun";
 	  }
 	  
-	  @GetMapping("/editnoun/{id}1")
-	  public String editnoun(@PathVariable("id") Long id) throws Exception {
+	  //POST requests to "/noun/editnoun". Handles editing an existing Noun in the data store
+	  @PostMapping("/editnoun")
+	  public String editnoun(Noun noun, BindingResult result, Model m) throws Exception {
 		  
-		  Noun editedNoun = repo.findById(id).get();
 		  
-		  //Validate what has been entered - alphabet only, lowercase, no spaces
 		  
-		  String regex = "([a-z])\\w*([^A-Z])*";
+		//Validate what has been entered - alphabet only, no spaces
+	  	  
+		  String regex = "([a-zA-Z])\\w*";
 		  
-		  if(!editedNoun.getEnglishNoun().matches(regex))
+		  if(!noun.getEnglishNoun().matches(regex))
 		  {
-			  throw new Exception("Invalid input for English noun. Only lowercase letters allowed");
+			  throw new Exception("Invalid input for English noun. Only letters allowed, no spaces");
 		  }
-		  else if(!editedNoun.getWelshNoun().matches(regex))
+		  else if(!noun.getWelshNoun().matches(regex))
 		  {
-			  throw new Exception("Invalid input for Welsh noun. Only lowercase letters allowed");
+			  throw new Exception("Invalid input for Welsh noun. Only letters allowed, no spaces");
 		  }
 		  
-		  repo.save(editedNoun);
+		  //Convert to lowercase
+		  noun.setEnglishNoun(noun.getEnglishNoun().toLowerCase());
+		  noun.setWelshNoun(noun.getWelshNoun().toLowerCase());
+		  
+		  
+		  repo.save(noun);
 		  
 		  return "redirect:/noun";
 	  
 	  }
 	  
+	  //GET requests to "/noun/editnoun/{nounID}. Handles initial request to edit a noun by presenting
+	  //the "editnoun" page to the user, prepopulated with the existing Noun details.
 	  @GetMapping("/editnoun/{id}")
 		public String nounEditPage(@PathVariable("id") Long id, Model m) {
 			m.addAttribute("noun", repo.findById(id).get());
